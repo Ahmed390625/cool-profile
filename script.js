facce8aabbd2b88d056954e8935eb7f4830f68fa
Ahmed390625 <81325833+Ahmed390625@users.noreply.github.com>
2023-10-11 09:35:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let tabs = document.querySelectorAll(".tabs li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content > div");
+let divsArray = Array.from(divs);
+
+// console.log(tabsArray);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    // console.log(ele);
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+    // console.log(e.currentTarget.dataset.cont);
+    document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
+  });
+});
+
+
+let ahmed = document.getElementById('ahmed')
+let ahmed2 = document.getElementById('ahmed2')
+
+function openlist() {
+  ahmed.style.top = '0px'
+
+}
+function closelist() {
+  ahmed.style.top = '-400px'
+  
+}
